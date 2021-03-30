716348d5a5b9d0b49d93b7cf2884e4a0d6ced5dd
Léon Muller <leon.muller@he-arc.ch>
2021-03-30 08:37:08 +0200

    It's done

--- Labo 2/fractal.js
+++ Labo 2/fractal.js
@@ -45,6 +45,15 @@ class Fractal{
     }
 }
 
+function limitValue(value, max)
+{
+    if (value > max)
+        return max;
+    else if (value < 0)
+        return 0;
+    else return value;
+}
+
 function load()
 {
     let canvas = document.getElementById("canvas");
@@ -53,15 +62,15 @@ function load()
 
     // Choose the color of the tree
     let rgb = []
-    rgb.push(document.getElementById("red").value.length > 0 ? parseInt(document.getElementById("red").value) % 255 : 150);
-    rgb.push(document.getElementById("green").value.length > 0 ? parseInt(document.getElementById("green").value)  % 255 : 50);
-    rgb.push(document.getElementById("blue").value.length > 0 ? parseInt(document.getElementById("blue").value) % 255 : 0);
+    rgb.push(document.getElementById("red").value.length > 0 ? limitValue(parseInt(document.getElementById("red").value), 255) : 150);
+    rgb.push(document.getElementById("green").value.length > 0 ? limitValue(parseInt(document.getElementById("green").value), 255) : 50);
+    rgb.push(document.getElementById("blue").value.length > 0 ? limitValue(parseInt(document.getElementById("blue").value), 255) : 0);
 
-    let size = document.getElementById("size").value.length > 0 ? parseInt(document.getElementById("size").value) % 100 : 100;
-    let thickness = document.getElementById("thickness").value.length > 0 ? parseInt(document.getElementById("thickness").value) % 50 : 10;
-    let nbBranch = document.getElementById("nbBranch").value.length > 0 ? parseInt(document.getElementById("nbBranch").value) % 40 : 25;
+    let size = document.getElementById("size").value.length > 0 ? limitValue(parseInt(document.getElementById("size").value), 150) : 100;
+    let thickness = document.getElementById("thickness").value.length > 0 ? limitValue(parseInt(document.getElementById("thickness").value), 50) : 10;
+    let nbBranch = document.getElementById("nbBranch").value.length > 0 ? limitValue(parseInt(document.getElementById("nbBranch").value), 255) : 25;
 
-    root = new Fractal(500, 500, nbBranch, size, thickness, 0, 0, context2D, rgb);
+    root = new Fractal(500, 650, nbBranch, size, thickness, 0, 0, context2D, rgb);
     root.draw()
 }
 
@@ -70,9 +79,9 @@ function randomize()
     document.getElementById("red").value = Math.floor(Math.random() * Math.floor(256));
     document.getElementById("green").value = Math.floor(Math.random() * Math.floor(256));
     document.getElementById("blue").value = Math.floor(Math.random() * Math.floor(256));
-    document.getElementById("size").value = Math.floor(Math.random() * Math.floor(50)) + 50;
+    document.getElementById("size").value = Math.floor(Math.random() * Math.floor(100)) + 50;
     document.getElementById("thickness").value = Math.floor(Math.random() * Math.floor(20));
-    document.getElementById("nbBranch").value = Math.floor(Math.random() * Math.floor(20)) + 10;   
+    document.getElementById("nbBranch").value = Math.floor(Math.random() * Math.floor(25)) + 5;   
 
     load();
 }
